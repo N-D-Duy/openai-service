cf3450dbd8d58070d52a056122f86362c250f9f4
N-D-Duy <Ct060311@actvn.edu.vn>
2025-06-27 17:54:16 +0700

    feat: update k8s config files

--- openai_app/urls.py
+++ openai_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import openai_chat, diagnose_disease, get_symptoms_list, get_diseases_by_department, demo_page
+from .views import openai_chat, diagnose_disease, get_symptoms_list, get_diseases_by_department
 
 urlpatterns = [
     path('chat/', openai_chat, name='openai_chat'),
-    path('diagnose/', diagnose_disease, name='diagnose_disease'),
+    path('diagnose/', diagnose_disease, name='diagnose_disease'),   
     path('symptoms/', get_symptoms_list, name='get_symptoms_list'),
     path('diseases/', get_diseases_by_department, name='get_diseases_by_department'),
-    path('demo/', demo_page, name='demo_page'),
 ]
